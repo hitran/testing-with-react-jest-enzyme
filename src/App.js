624ac701558e7904cb486330063b9d6cb87d3a5f
Hi Tran <thuchitran@gmail.com>
2020-08-14 09:58:31 +0700

    add redux to app, add success reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+
 import './App.css';
 import Router from './Router/Router'
 import { BrowserRouter } from 'react-router-dom';
+import store from './configureStore'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Router />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Router />
+        </div>
+      </Provider>
     </BrowserRouter>
   );
 }
